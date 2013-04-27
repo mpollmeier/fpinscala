1099986f0ae1a0acfaff03bf142b3a90b1ebd86c
Michael Pollmeier <michael@michaelpollmeier.com>
2013-04-27 19:02:34 +1200

    Tree functions

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -4,10 +4,28 @@ sealed trait Tree[+A]
 case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
-
 object Tree {
 
+  def treeSize(t: Tree[_]): Int = t match {
+    case Leaf(_)      ⇒ 1
+    case Branch(l, r) ⇒ 1 + treeSize(l) + treeSize(r)
+  }
 
+  def maximum(t: Tree[Int]): Int = t match {
+    case Leaf(v)      ⇒ v
+    case Branch(l, r) ⇒ maximum(l) max maximum(r)
+  }
 
+  def depth(t: Tree[_]): Int = {
+    def go(t: Tree[_], count: Int): Int = t match {
+      case Leaf(_)      ⇒ count + 1
+      case Branch(l, r) ⇒ go(l, count + 1) max go(r, count + 1)
+    }
+    go(t, 0)
+  }
 
+  def map[A, B](t: Tree[A])(f: A ⇒ B): Tree[B] = t match {
+    case Leaf(a)      ⇒ Leaf(f(a))
+    case Branch(l, r) ⇒ Branch(map(l)(f), map(r)(f))
+  }
 }
---
+++ exercises/src/test/scala/fpinscala/datastructures/TreeSpec.scala
@@ -0,0 +1,30 @@
+package fpinscala.datastructures
+
+import scala.annotation.tailrec
+import fpinscala.Spec
+
+class TreeSpec extends Spec {
+  describe("Trees") {
+    import Tree._
+
+    it("get's the size") {
+      treeSize(Leaf(Unit)) should be(1)
+      treeSize(Branch(Leaf(Unit), Leaf(Unit))) should be(3)
+      treeSize(Branch(Leaf(Unit), Branch(Leaf(Unit), Leaf(Unit)))) should be(5)
+    }
+
+    it("get's the maximum") {
+      maximum(Branch(Leaf(2), Branch(Leaf(5), Leaf(4)))) should be(5)
+    }
+
+    it("get's the maximum patch length") {
+      depth(Leaf(2)) should be(1)
+      depth(Branch(Leaf(2), Branch(Leaf(5), Leaf(4)))) should be(3)
+    }
+
+    it("maps") {
+      map(Leaf(2))(_ + 1) should be(Leaf(3))
+      map(Branch(Leaf(2), Branch(Leaf(5), Leaf(4))))(_ * 2) should be(Branch(Leaf(4), Branch(Leaf(10), Leaf(8))))
+    }
+  }
+}
