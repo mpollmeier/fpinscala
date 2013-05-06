5c6dde83ff586ab57e05ac02707430d9746376e2
Michael Pollmeier <michael@movio.co>
2013-05-06 17:55:22 +1200

    fun with futures

---
+++ exercises/src/test/scala/fpinscala/FutureCompositionSpec.scala
@@ -0,0 +1,31 @@
+package fpinscala
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+import scala.concurrent.Future
+import scala.concurrent.Await
+
+class FutureCompositionSpec extends Spec {
+
+  describe("Futures") {
+    it("returns one future if true and falls back to other future if false") {
+
+      def destiny(f: Future[Boolean]): Future[Int] =
+        f.flatMap(if (_) Future { 42 } else Future { -1 })
+
+      Await.result(destiny(Future { true }), 1 second) should be(42)
+      Await.result(destiny(Future { false }), 1 second) should be(-1)
+
+      // problem: calls both Futures
+    }
+
+    it("returns one future if true and falls back to other future if false 2") {
+      def destiny(f: Future[Boolean]): Future[Int] = f.map(
+        if (_) { println("a"); 42 }
+        else { println("b"); Await.result(Future { -1 }, 1 second) })
+
+      Await.result(destiny(Future { true }), 1 second) should be(42)
+      Await.result(destiny(Future { false }), 1 second) should be(-1)
+    }
+  }
+}
